92629bec328fc5b4f5aedb0abb9cb31e890a3c3a
Stephen Brough <StephenR.Brough@gmail.com>
2025-05-10 14:45:53 -0600

    Attempt to always include carver plugin

--- plugins/generated_plugin_registrant.cc
+++ plugins/generated_plugin_registrant.cc
@@ -35,10 +35,10 @@ void PluginsApiRegisterPlugins(FlutterDesktopEngineRef engine) {
   AudioPlayersLinuxPluginCApiRegisterWithRegistrar(
       FlutterDesktopGetPluginRegistrar(engine, ""));
 #endif
-#if ENABLE_PLUGIN_CARVERX5_CNC
+// #if ENABLE_PLUGIN_CARVERX5_CNC
   CarverX5CncPluginCApiRegisterWithRegistrar(
       FlutterDesktopGetPluginRegistrar(engine, ""));
-#endif
+// #endif
 #if ENABLE_PLUGIN_SECURE_STORAGE
   SecureStoragePluginCApiRegisterWithRegistrar(
       FlutterDesktopGetPluginRegistrar(engine, ""));
